11449fec177847885d9ff6095868c811056ee1f4
Oliver Beckstein <orbeckst@gmail.com>
2026-01-07 23:17:05 -0700

    data and notebooks
    - model_processing: clean up AF3 model and reorient
    - proton_pathway: use HOLLOW + networkx to find proton path between NHE6 D260 and TBC1D5 H207
    - diffusion: solve 1D Laplace equation for proton diffusion

---
+++ proton_pathway/RUN_hollow.sh
@@ -0,0 +1,5 @@
+# Install hollow from https://github.com/Becksteinlab/hollow in a Python 2.7 environment
+# mamba activate bp
+
+hollow -g 1.0 -c constraint.txt -o hollow.pdb ../model_processing/rotated.pdb
+
